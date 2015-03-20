23e50e18c809ccc9fcdae813b4dab731e8e5f1e3
Franck Silvestre <franck.silvestre@ticetime.com>
2015-03-20 09:56:18 +0100

    Correction TP1 - ex4 - correction q2

---
+++ grails-app/services/friendsofmine/JeuTestService.groovy
@@ -0,0 +1,23 @@
+package friendsofmine
+
+import grails.transaction.Transactional
+
+@Transactional
+class JeuTestService {
+
+    ActiviteService activiteService
+
+    def createJeuTestFoActivite() {
+        if (Activite.count() == 0) {
+            Utilisateur unResponsable = new Utilisateur(
+                    nom: "Dufrene",
+                    prenom: "Isabelle",
+                    sexe: "F",
+                    email: "df@df.org"
+            )
+            activiteService.insertOrUpdateActiviteForResponsable(new Activite(titre:"Act1"),unResponsable)
+            activiteService.insertOrUpdateActiviteForResponsable(new Activite(titre:"Act2"),unResponsable)
+            activiteService.insertOrUpdateActiviteForResponsable(new Activite(titre:"Act3"),unResponsable)
+        }
+    }
+}
