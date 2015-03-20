f0bb23bf260332a8abcbb4485c45534aee025458
Franck Silvestre <franck.silvestre@ticetime.com>
2015-03-20 11:21:14 +0100

    Correction TP1 - ex4 - correction q3

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,12 +1,17 @@
+import friendsofmine.JeuTestService
 import friendsofmine.Utilisateur
 
 class BootStrap {
 
+    JeuTestService jeuTestService
+
     def init = { servletContext ->
 
-        new Utilisateur(nom: "Dupond", prenom:"Jeanne", sexe:"F", email: "jd@jd.com",dateNaissance: new Date(1972,06,17)).save()
-        new Utilisateur(nom: "Dupond", prenom:"Jacques", sexe:"M", email: "jad@jad.com",dateNaissance: new Date(1973,06,17)).save()
-        new Utilisateur(nom: "Durand", prenom:"Viviana", sexe:"F", email: "viv@viv.com",dateNaissance: new Date(1990,06,17)).save()
+        new Utilisateur(nom: "Dupond", prenom: "Jeanne", sexe: "F", email: "jd@jd.com", dateNaissance: new Date(1972, 06, 17)).save()
+        new Utilisateur(nom: "Dupond", prenom: "Jacques", sexe: "M", email: "jad@jad.com", dateNaissance: new Date(1973, 06, 17)).save()
+        new Utilisateur(nom: "Durand", prenom: "Viviana", sexe: "F", email: "viv@viv.com", dateNaissance: new Date(1990, 06, 17)).save()
+
+        jeuTestService.createJeuTestFoActivite()
 
     }
     def destroy = {
