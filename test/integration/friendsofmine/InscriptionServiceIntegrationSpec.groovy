5d27a08064a02e9c07ab06ae903fc28ec7de1883
Franck Silvestre <franck.silvestre@ticetime.com>
2015-03-25 19:01:49 +0100

    TP4 - Ex3 -q4 on going

--- test/integration/friendsofmine/InscriptionServiceIntegrationSpec.groovy
+++ test/integration/friendsofmine/InscriptionServiceIntegrationSpec.groovy
@@ -10,6 +10,7 @@ class InscriptionServiceIntegrationSpec extends Specification {
 
     Activite uneActivite
     Utilisateur unUtilisateur
+    JeuTestService jeuTestService
 
     ActiviteService activiteService
     InscriptionService inscriptionService
@@ -67,5 +68,59 @@ class InscriptionServiceIntegrationSpec extends Specification {
         Activite.findById(uneActivite.id) != null
         Utilisateur.findById(unUtilisateur.id) != null
 
+    }
+
+    void "test du moteur de recherche sur les inscriptions (avec criteria)"() {
+
+        given:"les activités, les utilisateurs et les inscriptions fournis par le jeu de test "
+        jeuTestService
+
+        when:"on cherche les inscriptions dont le titre de l'activité contient 'ct' "
+        List<Inscription> res = inscriptionService.searchInscriptionsWithCriteria("ct",null,null)
+
+        then:"on récupère toutes les inscriptions"
+        res.size() == 3
+
+        when:"on cherche les inscriptions dont les activités sont gérées par le responsable dont le nom ou le prenom contient 'Val'"
+        res = inscriptionService.searchInscriptionsWithCriteria(null,'Val',null)
+
+        then:"on récupère uniquement l'inscription jacquesOnAct3"
+        res.size() == 1
+        res.contains(jeuTestService.jacquesOnAct3)
+
+        when:"on cherche les inscriptions sur lesquelles une personne dont le nom ou me prénom contient 'Jack' "
+        res = inscriptionService.searchInscriptionsWithCriteria(null,null,'Jack')
+
+        then:"on recupère les 2 inscriptions de Jacques"
+        res.size() == 2
+        res.contains(jeuTestService.jacquesOnAct3)
+        res.contains(jeuTestService.jacquesOnAct1)
+    }
+
+    void "test du moteur de recherche sur les inscriptions (avec HQL)"() {
+
+        given:"les activités, les utilisateurs et les inscriptions fournis par le jeu de test "
+        jeuTestService
+
+        when:"on cherche les inscriptions dont le titre de l'activité contient 'ct' "
+        List<Inscription> res = inscriptionService.searchInscriptionsWithHQL("ct",null,null)
+
+        then:"on récupère toutes les inscriptions"
+        res.size() == 3
+
+        when:"on cherche les inscriptions dont les activités sont gérées par le responsable dont le nom ou le prenom contient 'Val'"
+        res = inscriptionService.searchInscriptionsWithHQL(null,'Val',null)
+
+        then:"on récupère uniquement l'inscription jacquesOnAct3"
+        res.size() == 1
+        res.contains(jeuTestService.jacquesOnAct3)
+
+        when:"on cherche les inscriptions sur lesquelles une personne dont le nom ou me prénom contient 'Jack' "
+        res = inscriptionService.searchInscriptionsWithHQL(null,null,'Jack')
+
+        then:"on recupère les 2 inscriptions de Jacques"
+        res.size() == 2
+        res.contains(jeuTestService.jacquesOnAct3)
+        res.contains(jeuTestService.jacquesOnAct1)
     }
 }
