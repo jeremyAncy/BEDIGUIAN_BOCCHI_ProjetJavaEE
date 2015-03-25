d367606c49b40877d577a22e4d3353adeac6f8c3
Franck Silvestre <franck.silvestre@ticetime.com>
2015-03-25 18:45:11 +0100

    TP4 - Ex3 -q1,2,3 OK

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -7,19 +7,9 @@ class BootStrap {
 
     def init = { servletContext ->
 
-        try {
-            new Utilisateur(nom: "Dupond", prenom: "Jeanne", sexe: "F", email: "jd@jd.com", dateNaissance: new Date(1972, 06, 17)).save()
-            new Utilisateur(nom: "Dupond", prenom: "Jacques", sexe: "M", email: "jad@jad.com", dateNaissance: new Date(1973, 06, 17)).save()
-            new Utilisateur(nom: "Durand", prenom: "Viviana", sexe: "F", email: "viv@viv.com", dateNaissance: new Date(1990, 06, 17)).save()
-        } catch(Exception e) {
-            log.error(e.message)
-        }
-
-        try {
-            jeuTestService.createJeuTestFoActivite()
-        } catch (Exception e) {
-            log.error(e.message)
-        }
+        jeuTestService.createJeuTestForUtilisateurs()
+        jeuTestService.createJeuTestFoActivite()
+        jeuTestService.createJeuTestForInscription()
 
     }
     def destroy = {
